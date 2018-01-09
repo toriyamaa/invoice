041afe60099a146631367cba97797e5b96ac786c
toriyamaa <toriyamaa@systena.co.jp>
2018-01-10 02:19:11 +0900

    invoice-api　新規作成

---
+++ invoice-api/src/main/java/invoice/api/InvoiceApiApplication.java
@@ -0,0 +1,12 @@
+package invoice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceApiApplication.class, args);
+	}
+}

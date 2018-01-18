431b9f3d99f24f9b4010fa13346820009a9f8867
toriyamaa <toriyamaa@systena.co.jp>
2018-01-18 22:08:58 +0900

    新たにプロジェクト作成しなおしたので古いプロジェクト削除

--- invoice-dao/src/main/java/invoice/dao/repository/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package invoice.dao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import invoice.dao.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
-
-}
